59baef936a45c4c5eb4fb78584fbefe64f9bede3
Romit <romitamgai@lftechnology.com>
2016-08-21 03:21:49 +0545

    fix

--- frontend/src/js/components/body/post/ViewPost.js
+++ frontend/src/js/components/body/post/ViewPost.js
@@ -6,12 +6,15 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
+import showdown from 'showdown';
+
 //Components
 import Slider from '../home/slider-banner/Slider';
 
 class ViewPost extends Component {
   constructor(props) {
     super(props);
+    this.renderText = this.renderText.bind(this);
     this.state = {
       selectedPost: null
     }
@@ -33,6 +36,10 @@ class ViewPost extends Component {
     }
   }
 
+  renderText(text) {
+    return new showdown.Converter().makeHtml(text);
+  }
+
   render() {
     let description = this.state.selectedPost && this.state.selectedPost.description;
     return (
@@ -40,8 +47,7 @@ class ViewPost extends Component {
         <Slider imageUrl={this.state.selectedPost && this.state.selectedPost.bannerUrl}
                 postInfo={this.state.selectedPost}/>
         <div className="col s12 m12 l7">
-          <p className="flow-text post-text"
-             style={{whiteSpace:"pre-line"}}>{description || 'No Description Available'}</p>
+          <div className="flow-text post-text" dangerouslySetInnerHTML={{__html:this.renderText(description)}}/>
         </div>
         <div className="col l4 right .hide-on-med-and-down">
           <div className="featured-post">
@@ -56,7 +62,7 @@ class ViewPost extends Component {
               </div>
               <div className="card-content">
                 <p>Some Lorem ipsum goes here and it will be a long text of featured post.</p>
-                <div className="post-end"><span className="create-date">Bishrant Tandukar</span><span>admin</span></div>
+                <div className="post-end"><span className="create-date">27 August, 2015</span><span>admin</span></div>
               </div>
 
             </div>
