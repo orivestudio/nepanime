f034cf25b1ed6ae6c20ede1251def8bdf8c90a85
Romit <romitamgai@lftechnology.com>
2016-08-24 19:40:10 +0545

    dist push

--- frontend/src/js/components/wysiwyg/wysiwyg.js
+++ frontend/src/js/components/wysiwyg/wysiwyg.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Editor from './Editor';
-import showdown from 'showdown';
+import Showdown from 'showdown';
+import youtube from 'showdown-youtube';
 
 class wysiwyg extends Component {
 
@@ -37,7 +38,7 @@ class wysiwyg extends Component {
     e.preventDefault();
     let downloadElement = document.createElement('a');
     let categories = this.separateAndTrim();
-    let textToDownload = `{"id":,"imageUrl":"${this.state.postImageUrl}","bannerUrl":"${this.state.bannerImageUrl}","categories":[${categories}],"title":"${this.state.title}","user":{"name":"Nepanime","profilePictureUrl":"img/profilethumb.jpg","type":"admin"},"description":\`${this.state.textValue}\`,"date":"${this.refs.date.value}"}`;
+    let textToDownload = `{"id":,"imageUrl":# asdasd"${this.state.postImageUrl}","bannerUrl":"${this.state.bannerImageUrl}","categories":[${categories}],"title":"${this.state.title}","user":{"name":"Nepanime","profilePictureUrl":"img/profilethumb.jpg","type":"admin"},"description":\`${this.state.textValue}\`,"date":"${this.refs.date.value}"}`;
     downloadElement.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(textToDownload));
     downloadElement.setAttribute('download', 'test.json');
     downloadElement.click();
@@ -52,7 +53,8 @@ class wysiwyg extends Component {
   }
 
   renderText(text) {
-    return new showdown.Converter().makeHtml(text);
+    this.markupConverter = new Showdown.Converter({extensions: ['youtube'],smoothLivePreview:true});
+    return this.markupConverter.makeHtml(text);
   }
 
   onTextChange(e) {
@@ -67,12 +69,12 @@ class wysiwyg extends Component {
       <div className="wysiwyg">
         <div className='row'>
           <h1>Post Editor</h1>
-          <div className="input-field col s12">
-            <input placeholder="Title of the post" id="title" type="text" name="title" value={this.state.title}
-                   onChange={this.onTextChange} className="validate"/>
-            <label htmlFor='title'>Title</label>
-          </div>
           <form className='col s12'>
+            <div className="input-field col s12">
+              <input placeholder="Title of the post" id="title" type="text" name="title" value={this.state.title}
+                     onChange={this.onTextChange} className="validate"/>
+              <label htmlFor='title'>Title</label>
+            </div>
             <div className="input-field col s6">
               <input placeholder="Post Image URL" id="postImageUrl" type="text" name="postImageUrl" className="validate"
                      value={this.state.postImageUrl} onChange={this.onTextChange}/>
